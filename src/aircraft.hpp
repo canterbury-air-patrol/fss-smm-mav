6a4e8a40b157d5c4f3e27aeb22cfa532b3d02587
Scott Parlane <scott@parlanenz.com>
2021-04-02 22:23:24 +1300

    aircraft: Resolve issues identified by static analysis

--- src/aircraft.hpp
+++ src/aircraft.hpp
@@ -1,17 +1,20 @@
+#include <memory>
 #include <mutex>
 #include <map>
 #include "fmu-types.hpp"
+
 
 class aircraft_details {
     private:
         uint64_t ts{0};
         uint32_t icao_address;
+        static constexpr uint64_t ts_1sec_interval = 1000;
     public:
-        aircraft_details(uint32_t t_icao_address) : icao_address(t_icao_address) {};
-        uint32_t getICAOAddress() { return this->icao_address; };
-        bool acceptableUpdate(uint64_t t_new_timestamp)
+        explicit aircraft_details(uint32_t t_icao_address) : icao_address(t_icao_address) {};
+        auto getICAOAddress() -> uint32_t { return this->icao_address; };
+        auto acceptableUpdate(uint64_t t_new_timestamp) -> bool
         {
-            if(this->ts + 1000 <= t_new_timestamp)
+            if(this->ts + this->ts_1sec_interval <= t_new_timestamp)
             {
                 this->ts = t_new_timestamp;
                 return true;
@@ -24,9 +27,10 @@ class known_aircraft {
     private:
         std::mutex lock{};
         /* Warning: This is part of an "Unallocated" range of ICAO callsigns */
-        uint32_t lastAllocatedICAO{0x1000};
-        std::map<std::string, aircraft_details *> aircraft{};
-        aircraft_details *findAircraft(std::string t_call_sign, uint32_t t_icao_address)
+        static constexpr uint32_t first_icao_address_for_unknown_aircraft = 0x1000;
+        uint32_t lastAllocatedICAO{first_icao_address_for_unknown_aircraft};
+        std::map<std::string, std::shared_ptr<aircraft_details>> aircraft{};
+        auto findAircraft(const std::string &t_call_sign, uint32_t t_icao_address) -> std::shared_ptr<aircraft_details>
         {
             auto result = this->aircraft.find(t_call_sign);
             if (result != this->aircraft.end())
@@ -37,20 +41,13 @@ class known_aircraft {
             {
                 t_icao_address = ++this->lastAllocatedICAO;
             }
-            auto ad = new aircraft_details(t_icao_address);
-            this->aircraft.insert(std::pair<std::string, aircraft_details *>(t_call_sign, ad));
+            auto ad = std::make_shared<aircraft_details>(t_icao_address);
+            this->aircraft.insert(std::pair<std::string, std::shared_ptr<aircraft_details>>(t_call_sign, ad));
             return ad;
         }
     public:
-        known_aircraft() {};
-        ~known_aircraft() {
-            for (auto ad : this->aircraft)
-            {
-                delete ad.second;
-            }
-            this->aircraft.clear();
-        };
-        bool newPositionReport(PositionData &pd)
+        known_aircraft() = default;
+        auto newPositionReport(PositionData pd) -> bool
         {
             std::lock_guard<std::mutex> lk(this->lock);
             auto ad = this->findAircraft(pd.getCallSign(), pd.getICAOAddress());
